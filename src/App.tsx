a1751c95fe9e6c122d2261779748e78057ffc87d
mughilan16 <mughilant@proton.me>
2024-07-02 15:18:37 +0530

    fix cancel

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
 import axios from "axios";
 import { useMutation } from "react-query"
 import { saveAs } from 'file-saver';
+import { useRef } from "react";
 
 function App() {
-  const controller = new AbortController();
-  const url = 'http://ec2-100-21-24-56.us-west-2.compute.amazonaws.com:8080/antares-backend/public/api/file-download/pre-edit+clean-up+userid-1066+20240617093812.zip?fileName=1234567_clean_up.zip'
-  const signal = controller.signal;
-  const { mutateAsync: downlaod, isLoading } = useMutation('dataiahgoe', async () => {
-    await axios.get(url, { responseType: 'blob', signal: signal }).then((response) => {
-      console.log(response.data)
-      saveAs(response.data, "zipfil.zip");
-    }).then(_ => {
-      alert("file downloaded")
-    })
-      .catch(() => {
-        alert("file download failed")
-      });
-  })
+  const controllerRef = useRef<AbortController>();
 
-  console.log(isLoading)
+  const url = 'http://ec2-100-21-24-56.us-west-2.compute.amazonaws.com:8080/antares-backend/public/api/file-download/pre-edit+clean-up+userid-1066+20240617093812.zip?fileName=1234567_clean_up.zip'
+  const { mutateAsync: download, isLoading } = useMutation('zipfile', async () => {
+    controllerRef.current = new AbortController();
+    await axios.get(url, { responseType: 'blob', signal: controllerRef.current.signal }).then(res => {
+      saveAs(res.data)
+    }),
+    {
+      onError: () => {
+        alert("file download failed");
+      },
+      onSuccess: () => {
+        alert("file downloaded");
+      }
+    }
+  })
 
   return (
     <div>
-      <button onClick={() => downlaod()}>Download</button>
+      <button onClick={() => download()}>Download</button>
       {isLoading && "downloading"}
-      <button onClick={() => controller.abort()}>Cancel</button>
-    </div>
+      <button onClick={() => controllerRef.current?.abort()}>Cancel</button>
+    </div >
   )
 
 }
